e705919e6bf1eaa23508891b8656adca47efc5b5
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-05-13 21:46:13 -0400

    pong starter

--- src/hello_quil/app.cljs
+++ src/hello_quil/app.cljs
@@ -1,6 +1,15 @@
 (ns hello-quil.app
   (:require [quil.core :as q :include-macros true]))
 
+(def width 500)
+(def height 500)
+(def paddle-width 20)
+(def paddle-height 100)
+(def ball-diameter 30)
+
+(defn v []
+  ((if (> (rand) 0.5) + -) (inc (rand-int 4))))
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
@@ -8,25 +17,55 @@
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; circle color and position.
-  {:color 0
-   :angle 0})
+  {:player1 {:y 0}
+   :player2 {:y 0}
+   :ball {:x (/ width 2)
+          :y (/ height 2)
+          :dx (v)
+          :dy (v)}})
+
+(defn draw-paddle [x y]
+  (q/rect x y paddle-width paddle-height))
+
+(defn draw-paddles [player1 player2]
+  (draw-paddle 10 (:y player1))
+  (draw-paddle (- (q/width) 30) (:y player2)))
+
+(defn draw-ball [{:keys [x y]}]
+  (q/ellipse (+ x (/ ball-diameter 2))
+             (+ y (/ ball-diameter 2))
+             ball-diameter
+             ball-diameter))
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  (update-in state [:ball]
+    (fn [{:keys [x y dx dy] :as ball}]
+      (if (and (< x width) (> x 0))
+        {:x (+ x dx)
+         :y (+ y dy)
+         :dx dx
+         :dy dy}
+        ball))))
 
-(defn draw-state [{:keys [color angle]}]
+(defn down [y]
+  (if (< y (- height paddle-height)) (+ y 10) y))
+
+(defn up [y]
+  (if (> y 0) (- y 10) y))
+
+(defn key-press [state {:keys [key]}]
+  (case key
+    :a    (update-in state [:player2 :y] up)
+    :z    (update-in state [:player2 :y] down)
+    :up   (update-in state [:player1 :y] up)
+    :down (update-in state [:player1 :y] down)
+    state))
+
+(defn draw-state [{:keys [ball player1 player2]}]
   ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
+  (q/background 0)
   ; Set circle color.
-  (q/fill color 255 255)
+  (q/fill 255 255 255)
   ; Calculate x and y coordinates of the circle.
-  (let [angle angle
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 200 100))))
+  (draw-paddles player1 player2)
+  (draw-ball ball))
--- src/hello_quil/core.cljs
+++ src/hello_quil/core.cljs
@@ -1,18 +1,24 @@
 (ns ^:figwheel-no-load hello-quil.core
   (:require [quil.core :as q :include-macros true]
             [quil.middleware :as m]
-            [hello-quil.app :refer [setup update-state draw-state]]))
+            [hello-quil.app :as app]))
+
+(enable-console-print!)
 
 (q/defsketch hello-quil
   :host "hello-quil"
-  :size [500 500]
+  :size [app/width app/height]
   ; setup function called only once, during sketch initialization.
-  :setup setup
+  :setup #'app/setup
   ; update-state is called on each iteration before draw-state.
-  :update #'update-state
-  :draw #'draw-state
+  :update #'app/update-state
+  :draw #'app/draw-state
+  :key-pressed #'app/key-press
+
   ; This sketch uses functional-mode middleware.
   ; Check quil wiki for more info about middlewares and particularly
   ; fun-mode.
+
+  :features [:global-key-events]
   :middleware [m/fun-mode])
 
