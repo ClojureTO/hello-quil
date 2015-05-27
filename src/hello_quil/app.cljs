c798bb35b1a6a20eb8560548c56d35f48c030c24
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-05-26 20:37:42 -0400

    updated ball movement logic

--- src/hello_quil/app.cljs
+++ src/hello_quil/app.cljs
@@ -5,7 +5,8 @@
 (def height 500)
 (def paddle-width 20)
 (def paddle-height 100)
-(def ball-diameter 30)
+(def radius 15)
+
 
 (defn v []
   ((if (> (rand) 0.5) + -) (inc (rand-int 4))))
@@ -17,16 +18,16 @@
   (q/color-mode :hsb)
   ; setup function returns initial state. It contains
   ; the player paddles and the ball
-  (let[mid-x     (/ width 2)
-       mid-y     (/ height 2)
-       initial-y (- mid-x (/ paddle-height 2))]
+  (let [mid-x (/ width 2)
+        mid-y (/ height 2)
+        initial-y (- mid-x (/ paddle-height 2))]
     {:player1 {:y initial-y}
      :player2 {:y initial-y}
-     :keys #{}
-     :ball {:x (- mid-x (/ ball-diameter 2))
-            :y mid-y
-            :dx (v)
-            :dy (v)}}))
+     :keys    #{}
+     :ball    {:x  (- mid-x radius)
+               :y  (- mid-y radius)
+               :dx (v)
+               :dy (v)}}))
 
 (defn draw-paddle [x y]
   (q/rect x y paddle-width paddle-height))
@@ -36,10 +37,7 @@
   (draw-paddle (- (q/width) 30) (:y player2)))
 
 (defn draw-ball [{:keys [x y]}]
-  (q/ellipse (+ x (/ ball-diameter 2))
-             (+ y (/ ball-diameter 2))
-             ball-diameter
-             ball-diameter))
+  (q/ellipse x y (* 2 radius) (* 2 radius)))
 
 (defn down [y]
   (if (< y (- height paddle-height)) (+ y 10) y))
@@ -49,26 +47,44 @@
 
 (defn update-player-positions [state]
   (reduce
-   (fn [state key]
-     (case key
-       :a    (update-in state [:player2 :y] up)
-       :z    (update-in state [:player2 :y] down)
-       :up   (update-in state [:player1 :y] up)
-       :down (update-in state [:player1 :y] down)
-       state))
-   state (:keys state)))
+    (fn [state key]
+      (case key
+        :a (update-in state [:player2 :y] up)
+        :z (update-in state [:player2 :y] down)
+        :up (update-in state [:player1 :y] up)
+        :down (update-in state [:player1 :y] down)
+        state))
+    state (:keys state)))
+
+(defn update-velocity [v d max]
+  (if (or
+        (and (pos? v) (> d (- max radius)))
+        (and (neg? v) (< d radius)))
+    (* -1 v) v))
+
+(defn update-ball-v [state]
+  (update-in
+    state
+    [:ball]
+    (fn [{:keys [x y dx dy] :as ball}]
+      (assoc ball
+        :dx (update-velocity dx x width)
+        :dy (update-velocity dy y height)))))
+
+(defn update-ball-xy [state]
+  (update-in
+    state
+    [:ball]
+    (fn [{:keys [x y dx dy] :as ball}]
+      (assoc ball
+        :x (+ x dx)
+        :y (+ y dy)))))
 
 (defn update-state [state]
   (-> state
       update-player-positions
-      (update-in
-       [:ball]
-       (fn [{:keys [x y dx dy]}]
-         (if (and (< x width) (> x 0))
-           {:x (+ x dx)
-            :y (+ y dy)
-            :dx dx
-            :dy dy})))))
+      update-ball-v
+      update-ball-xy))
 
 (defn key-press [state {:keys [key]}]
   (if (some #{key} [:a :z :up :down])
